eea4e719890378877fed9ec198418b0938c3689d
Woody Wu <Woody Wu>
2016-04-30 19:08:21 +0800

    upload test9

--- submitteam.php
+++ submitteam.php
@@ -98,7 +98,7 @@
 			$filename = mysqli_insert_id($conn).'.'.$extend;
 			//move_uploaded_file($_FILES['leaderpic']['tmp_name'], $_ENV['OPENSHIFT_REPO_DIR'].'leaderpictures/'.$filename)
 			move_uploaded_file($_FILES['leaderpic']['tmp_name'], $_ENV['OPENSHIFT_DIR_DIR'].'leaderpictures/'.$filename);
-			move_uploaded_file($_FILES['leaderpic']['tmp_name'], $_ENV['OPENSHIFT_REPO_DIR'].'leaderpictures/'.$filename);
+			copy($_ENV['OPENSHIFT_DIR_DIR'].'leaderpictures/'.$filename, $_ENV['OPENSHIFT_REPO_DIR'].'leaderpictures/'.$filename);
 		}else{
 			$filename = substr($_FILES['leaderpic']['name'], 0,strrpos($_FILES['leaderpic']['name'], "."));
 			$extend = substr($_FILES['leaderpic']['name'], strrpos($_FILES['leaderpic']['name'], ".")+1,strlen($_FILES['leaderpic']['name'])-strrpos($_FILES['leaderpic']['name'], ".")-1);
@@ -109,7 +109,7 @@
 			$filename = $row['id'].'.'.$extend;
 			//move_uploaded_file($_FILES['leaderpic']['tmp_name'], $_ENV['OPENSHIFT_REPO_DIR'].'leaderpictures/'.$filename);
 			move_uploaded_file($_FILES['leaderpic']['tmp_name'], $_ENV['OPENSHIFT_DATA_DIR'].'leaderpictures/'.$filename);
-			move_uploaded_file($_FILES['leaderpic']['tmp_name'], $_ENV['OPENSHIFT_REPO_DIR'].'leaderpictures/'.$filename);
+			copy($_ENV['OPENSHIFT_DIR_DIR'].'leaderpictures/'.$filename, $_ENV['OPENSHIFT_REPO_DIR'].'leaderpictures/'.$filename);
 		}
 	}
 	for($i = 0;$i < $_POST['memberamount'];$i++){
@@ -136,7 +136,7 @@
 			$filename = mysqli_insert_id($conn).'.'.$extend;
 			//move_uploaded_file($_FILES['memberpic'.($i+1)]['tmp_name'], $_ENV['OPENSHIFT_REPO_DIR'].$dir.'/'.$filename);
 			move_uploaded_file($_FILES['memberpic'.($i+1)]['tmp_name'], $_ENV['OPENSHIFT_DATA_DIR'].$dir.'/'.$filename);
-			move_uploaded_file($_FILES['memberpic'.($i+1)]['tmp_name'], $_ENV['OPENSHIFT_REPO_DIR'].'memberpictures/'.$filename);
+			copy($_ENV['OPENSHIFT_DATA_DIR'].$dir.'/'.$filename,$_ENV['OPENSHIFT_REPO_DIR'].$dir.'/'.$filename);
 		}else{
 			$sql = "update teammember set name='".$current->name."', number='".$current->number."', departlevel='".$current->departlevel."', participate1='".$current->participate1."', participate2='".$current->participate2."', field1='".$current->field2."' where id=".$current->id;
 			$conn->query($sql);
@@ -155,7 +155,7 @@
 				$filename = $picid.'.'.$extend;
 				//move_uploaded_file($_FILES['memberpic'.($i+1)]['tmp_name'], $_ENV['OPENSHIFT_REPO_DIR'].$dir.'/'.$filename);
 				move_uploaded_file($_FILES['memberpic'.($i+1)]['tmp_name'], $_ENV['OPENSHIFT_DATA_DIR'].$dir.'/'.$filename);
-				move_uploaded_file($_FILES['memberpic'.($i+1)]['tmp_name'], $_ENV['OPENSHIFT_REPO_DIR'].$dir.'/'.$filename);
+				copy($_ENV['OPENSHIFT_DATA_DIR'].$dir.'/'.$filename,$_ENV['OPENSHIFT_REPO_DIR'].$dir.'/'.$filename);
 			}
 		}
 	}
